ad6353c9df5ba43b3bd5b4642f2b99ac6ec3d1a4
pavon <pavongomezdaniel@gmail.com>
2022-01-02 15:03:09 +0100

    dont know nothing

--- main.c
+++ main.c
@@ -1,77 +1,65 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 
 typedef	struct s_dates
-{
-	pthread_mutex_t     *mutex;
+{	
+	pthread_mutex_t     fork1;
+	pthread_mutex_t     fork2;
 	int					contador;
 	int					i;
 }	t_dates;
 
-void    *function1(void *args)
+void    *birth_philo(void *args)
 {
 	t_dates *dates;
+	int		total;
 
+	total = 0;
 	dates = (t_dates*)args;
-	int	i;
 
-	i = 0;
-	while (i < 10000)
-	{
-		pthread_mutex_lock(&dates->mutex[0]);
-		i++;
-		dates->contador++;
-		pthread_mutex_unlock(&dates->mutex[0]);
-	}
-}
+	// usleep(1000 * dates->i);
 
-void    *function2(void *args)
-{
-	t_dates *dates;
-
-	dates = (t_dates*)args;
-	int	i;
-
-	i = 0;
-	while (i < 10000)
-	{
-		pthread_mutex_lock(&dates->mutex[0]);
-		i++;
-		dates->contador--;
-		pthread_mutex_unlock(&dates->mutex[0]);
-	}
+	// pthread_mutex_lock(&dates->fork1);
+	printf("Philo %d\n", dates->i);
+	// pthread_mutex_unlock(&dates->fork1);
+	pthread_mutex_unlock(&dates->fork2);
+	
 }
 
 int main()
 {
 	t_dates		dates;
 	pthread_t	*hilos;
-	int			number;
 	int			i;
+	int			number;
 
-	dates.i = 0;
-	number = 4;
+	number = 2;
 	dates.contador = 0;
-	dates.mutex = malloc(sizeof(pthread_mutex_t) * number);
-	while (dates.i < number)
-	{
-		pthread_mutex_init(&dates.mutex[dates.i], NULL);
-		dates.i++;
-	}
+
+	pthread_mutex_init(&dates.fork1, NULL);
+	pthread_mutex_init(&dates.fork2, NULL);
 	hilos = malloc(sizeof(pthread_t) * number);
 	dates.i = 0;
 	while (dates.i < number)
 	{
-		pthread_create(&hilos[dates.i], NULL, function1, (void *)&dates);
+		pthread_mutex_lock(&dates.fork2);
+		pthread_create(&hilos[dates.i], NULL, birth_philo, (void *)&dates);
 		dates.i++;
 	}
+
 	dates.i = 0;
-	while (dates.i < number)
-	{
-		pthread_join(hilos[dates.i], NULL);
-		dates.i++;
-	}
+	// while (dates.i < number)
+	// {
+	// 	pthread_join(hilos[dates.i], NULL);
+	// 	dates.i++;
+	// }
+	pthread_join(hilos[0], NULL);
+	pthread_join(hilos[1], NULL);
+	// pthread_join(hilos[2], NULL);
+	// pthread_join(hilos[3], NULL);
+	// pthread_join(hilos[4], NULL);
 	free(hilos);
-	printf("Valor del contador: %d\n", dates.contador);
 }
